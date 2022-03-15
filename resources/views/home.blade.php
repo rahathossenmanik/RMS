71bece0a5f504b6bb05bc1231cffbb9d351fcb35
Manik Hosen <rahathossenmanik@gmail.com>
2022-03-15 13:19:47 +0600

    Laravel Project Created with Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Midway Dine - Your Favourite Foods</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/templatemo-klassy-cafe.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/owl-carousel.css')}}">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css')}}">
+
+    </head>
+
+    <body>
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <a href="{{url('home')}}" class="logo">
+                            Logo
+                        </a>
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about">About</a></li>
+
+                            <li class="scroll-to-section"><a href="#menu">Menu</a></li>
+                            <li class="scroll-to-section"><a href="#chefs">Chefs</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Features</a>
+                                <ul>
+                                    <li><a href="#">Features Page 1</a></li>
+                                    <li><a href="#">Features Page 2</a></li>
+                                    <li><a href="#">Features Page 3</a></li>
+                                    <li><a href="#">Features Page 4</a></li>
+                                </ul>
+                            </li>
+                            <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li>
+                            <li>
+                                @if (Route::has('login'))
+                                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                    @auth
+                                        <li style="margin-top:-13px;">
+                                            <x-app-layout> </x-app-layout>
+                                        </li>
+                                    @else
+                                      <li>
+                                        <a href="#" class="text-sm text-gray-700 underline">Log in</a>
+                                      </li>
+                                        @if (Route::has('register'))
+                                            <li><a href="#" class="ml-4 text-sm text-gray-700 underline">Register</a> </li>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                            </li>
+                        </ul>
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="top">
+        <div class="container-fluid">
+            Main Landing Section
+        </div>
+    </div>
+    <section class="section" id="about">
+        <div class="container">
+            About Section
+        </div>
+    </section>
+    <section class="section" id="menu">
+        <div class="container">
+            Menu Section
+        </div>
+    </section>
+    <section class="section" id="chefs">
+        <div class="container">
+            Chefs Section
+        </div>
+    </section>
+    <section class="section" >
+        <div class="container">
+            Booking Section
+        </div>
+    </section>
+    <section class="section" id="offers">
+        <div class="container">
+            Offers
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-xs-12">
+                    <div class="right-text-content">
+                            <ul class="social-icons">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="logo">
+                        <a href="{{url('home')}}">Logo</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-xs-12">
+                    <div class="left-text-content">
+                        <p>© Copyright RU CSE
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="{{ asset('assets/js/jquery-2.1.0.min.js')}}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{ asset('assets/js/popper.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
+
+    <!-- Plugins -->
+    <script src="{{ asset('assets/js/owl-carousel.js')}}"></script>
+    <script src="{{ asset('assets/js/accordions.js')}}"></script>
+    <script src="{{ asset('assets/js/datepicker.js')}}"></script>
+    <script src="{{ asset('assets/js/scrollreveal.min.js')}}"></script>
+    <script src="{{ asset('assets/js/waypoints.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{ asset('assets/js/imgfix.min.js')}}"></script>
+    <script src="{{ asset('assets/js/slick.js')}}"></script>
+    <script src="{{ asset('assets/js/lightbox.js')}}"></script>
+    <script src="{{ asset('assets/js/isotope.js')}}"></script>
+
+    <!-- Global Init -->
+    <script src="{{ asset('assets/js/custom.js')}}"></script>
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+              $("."+selectedClass).fadeIn();
+              $("#portfolio").fadeTo(50, 1);
+            }, 500);
+
+            });
+        });
+
+    </script>
+  </body>
+</html>
