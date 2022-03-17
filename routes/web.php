40ea654da251e72015909b4494da407c87f40251
Mahady Hasan <mahadyhasankbc710@gmail.com>
2022-03-17 13:32:50 +0600

    Menu Cntroller

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", 'App\Http\Controllers\HomeController@index');
 
+Route::get("/menu",'App\Http\Controllers\MenuController@menu');
+
 Route::get("/redirects",'App\Http\Controllers\HomeController@redirects');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
